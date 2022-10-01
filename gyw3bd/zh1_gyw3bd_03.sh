1ee01e7fc29985019873f4ae642cf72e1791b81a
Polcsi <polcsi@polcsi>
2022-10-01 20:38:56 +0200

    New third task solution

---
+++ gyw3bd/zh1_gyw3bd_03.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# zh1 harmadik feladat
+
+for FILE in ../../elements/*.html; do
+    echo "$FILE"
+    tableData=$(sed -n '/<table>/,/<\/table>/p' "$FILE" | sed '/body>/d' | sed 's/<[^>]*./;/g')
+    # other solution
+    #echo "cat //html/body/table" | xmllint --html --shell "$FILE" | sed '/^\/ >/d' | sed 's/<[^>]*.//g' | xargs
+    tableParts=(${tableData// / })
+    for i in "${tableParts[@]}"; do
+        rowData=(${i//;;/ })
+        ipParts=(${rowData[1]//./ })
+        if [[ "${ipParts[2]}" -lt "8" ]]; then
+            echo ${rowData[1]}
+        fi
+    done
+done
