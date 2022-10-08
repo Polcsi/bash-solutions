2dac98e2dbc3d64af28bd4b32eaae312651f823f
Polcsi <polcsi@polcsi>
2022-10-08 18:12:49 +0200

    update zh1_minta

--- zh1_minta/03.sh
+++ zh1_minta/03.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # harmadik feladat megoldás
 
-declare max
+declare maxTraffic
 declare ipAddress
 h="IPv4;Traffic"
-for FILE in *.csv; do
-    while read -r line; do
+for FILE in files/*.csv; do
+    while read line; do
         if [[ "$h" != "$line" ]]; then
             parts=(${line//;/ })
-            if [[ ${parts[1]} -gt $max ]]; then
-                let max=${parts[1]}
+            if [[ ${parts[1]} -gt $maxTraffic ]]; then
+                let maxTraffic=${parts[1]}
                 ipAddress="${parts[0]}"
             fi
         fi
     done <$FILE
 done
 
-echo "Legnagyobb öszforgalom itt történt: $ipAddress $max"
+echo "Legnagyobb öszforgalom itt történt: $ipAddress $maxTraffic"
