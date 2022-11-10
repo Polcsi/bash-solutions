07d66a110ef90ce575bbe0f0087eaeae1b1b6e0a
root <root@polcsi>
2022-11-10 22:37:53 +0100

    add new solution

---
+++ zh2_a1w2fu/02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Csak az év utolsó negyedévében született urakat jelenítse meg. (2)
+sed -nr '/\/úr\/[0-9]{4}\.1[012]\.[0-9]{2}\./p' adat
+
+# Az egy milliónál kevesebbet kereső urak esetében a sorszám elé rakjon egy “-” jelet. (2)
+sed -i '/\/úr\/.*\/[0-9]\{1,6\}$/s/^/-/' adat
+
+# Az év első harmadában született urak esetében a születési idő oszlopot módosítsa úgy, hogy csak a születési év látszódjon. (3)
+sed 's/\.[0-9]\{2\}\.[0-9]\{2\}\.//g' adat
