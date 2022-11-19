914ba7ba80980604b907d50f201da8d8119c0e10
root <root@polcsi>
2022-11-19 13:42:23 +0100

    new awk solution

--- zh2_evd61o/03.sh
+++ zh2_evd61o/03.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Írja ki azon legidősebb 4 hölgy adatait, akinek a nevében nem szerepel sem az “S”, sema “k” betű, és 500000-nél kevesebbet keresnek! Az egyszerűség kedvéért feltételezhetjük, hogy különböző napokon születtek. (awk felhasználásával) (3)
 
-awk -F\_ '
-BEGIN{}
+# Írja ki azon legidősebb 4 hölgy adatait, akinek a vezetéknevében szerepel az “a” betű, és 700000-nél nem keresnek többet! Az egyszerűség kedvéért feltételezh
+
+awk -F\! '
+BEGIN {}
 {
     if($3=="hölgy") {
-        if(match($2, "[Sk]+") == 0 && $6<500000) {
+        if(match($2, "[a]+") && $6 <= 700000) {
             result[$4] = $1" "$2" "$3" "$4" "$5" "$6
         }
     }
@@ -21,14 +22,14 @@ END {
 }
 ' adat
 
-# Melyik fizetés a leggyakoribb a Budapestiek körében? Listázzuk a budapesti személyek esetében előforduló fizetéseket, és ezek darabszámát fizetés szerint növekvő sorrendben. (awk felhasználásával) (3)
+#  Melyik fizetés a leggyakoribb a Budapestiek körében? Listázzuk a budapesti személyek esetében előforduló fizetéseket, és ezek darabszámát fizetés szerint növekvő sorrendben. (awk felhasználásával) (3)
 
-awk -F\_ '
-BEGIN { }
+awk -F\! '
+BEGIN {}
 {
- if($5 == "Budapest") {
-    salarys[$6] = $6
- }
+    if($5=="Budapest") {
+        salarys[$6] = $6
+    }
 }
 END {
     asort(salarys)
@@ -41,19 +42,18 @@ END {
 
 # Melyik vezetéknév a leggyakoribb az hölgyek körében? Írassuk ki az egyes vezetékneveket és az adott vezetéknévvel rendelkező hölgyek %-os arányát négy tizedes jegy pontossággal, %-os arány szerinti csökkenő sorrendben. (csak awk-t használva) (6)
 
-awk -F\_ '
+awk -F\! '
 BEGIN {}
 {
-    if($3 == "hölgy") {
-        split($2, name, " ")
-        firstNames[name[1]]++
+    if($3=="hölgy") {
+        split($2, nameParts, " ")
+        firstNames[nameParts[1]]++
         counter++
     }
 }
 END {
     for(i in firstNames) {
-       result[i]=(firstNames[i] / counter) * 100
-       namesResult=i
+        result[i]=(firstNames[i] / counter) * 100
     }
     for(j in result) {
         printf "%s, %.6s%\n", j, result[j]
