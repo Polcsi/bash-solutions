adad2eec09d53117d1dce60e0421e9944af55e6c
root <root@polcsi>
2022-11-20 22:02:12 +0100

    new solutions

--- Zh2_pigoes/02.sh
+++ Zh2_pigoes/02.sh
@@ -2,6 +2,12 @@
 
 # Az év negyedik negyedévében született hölgyek esetében szúrjon be egy üres sort az adott sor elé. (2)
 
+sed '/\%hölgy\%[0-9]\{4\}\.1[012]\./s/^/\n/' adat
+# sed '/\%hölgy\%[0-9]\{4\}\.1[012]\./s/$/\n/' adat
+
 # Az év utolsó harmadában született személyeket ne listázza ki. (2)
+sed -nr '/\%[0-9]{4}\.(0[^9]|1[^012])\./ {p}' adat
 
 # A nem Dunaújvárosi urak esetében cserélje meg a sorszám és a fizetés oszlopot. (3)
+
+sed -e '/<úr>/ !{/Dunaújváros/{n};s/(.[0-9][0-9].)(%[A-Z].)(%[0-9])$/\1\3\2/}' adat
--- Zh2_pigoes/03.sh
+++ Zh2_pigoes/03.sh
@@ -1,7 +1,54 @@
 #!/bin/bash
 
 #• Írja ki azon legidősebb 3 hölgy adatait, akinek a nevében szerepel az “S” és a “a” betű, és 600000-nél nem keresnek kevesebbet! Az egyszerűség kedvéért feltételezhetjük, hogy különböző napokon születtek. (awk felhasználásával) (3)
+awk -F\% '
+BEGIN {}
+{
+    if(match($2, "[S]+") > 0 && match($2, "[a]+") > 0 && $6 > 600000 && $3 == "hölgy") {
+        result[$4] = $1" "$2" "$3" "$4" "$5" "$6
+    }
+}
+END {
+    n=asorti(result, sorted)
+    for(i=1;i<n;i++) {
+        if(i==4) {
+            break
+        }
+        print result[sorted[i]]
+    }
+}
+' adat
 
 # Melyik hónapban született a legtöbb úr? Listázzuk az egyes hónapok sorszámait, és az akkor született urak számát, darabszám szerint csökkenő sorrendben. (awk felhasználásával) (3)
+awk -F\% '
+BEGIN{}
+{
+    if($3 == "úr") {
+        split($4, parts, ".")
+        result[parts[2]]++
+    }
+}
+END {
+    for(i in result) {
+        print i": "result[i]
+    }
+}
+' adat | sort -rk 2
 
 # Melyik vezetéknév a leggyakoribb az urak körében? Írassuk ki az egyes vezetékneveket és az adott vezetéknévvel rendelkező urak %-os arányát négy tizedes jegy pontossággal, %-os arány szerinti csökkenő sorrendben. (csak awk-t használva) (6)
+awk -F\% '
+BEGIN{}
+{
+    if($3=="úr") {
+        split($2, names, " ")
+        result[names[1]]++
+        counter++
+    }
+}
+END {
+    for(i in result) {
+        printf "%s: %.6s%\n", i, (result[i] / counter) * 100
+    }
+    print "Length: " counter
+}
+' adat | sort -rk 2
