0351d36430858b0f8ab22a6564150711488c58ac
root <root@polcsi>
2022-11-21 11:03:08 +0100

    removed unused comments

--- zh2_b35s1h/02.sh
+++ zh2_b35s1h/02.sh
@@ -5,7 +5,7 @@
 sed -ri '/\+úr\+[0-9]{4}\.0[1234]\.[0-9]{2}\./ {s/\+[0-9]+$//g}' adat
 
 #2.2 Az egy milliónál nem kevesebbet kereső hölgyek esetében a sorszám elé rakjon egy “+” jelet. (2)
-#sed -i '/\+hölgy\+.*\+[0-9]\{7,\}$/s/^/+/' adat
+sed -i '/\+hölgy\+.*\+[0-9]\{7,\}$/s/^/+/' adat
 
 #2.3 Az év utolsó harmadában született hölgyek esetében a születési idő oszlopot módosítsa úgy, hogy csak a születési év látszódjon. (3)
-#sed -i '/\<hölgy\>/ {s/\.\(09\|1[012]\)\.[0-9]\{2\}\.//g}' adat
+sed -i '/\<hölgy\>/ {s/\.\(09\|1[012]\)\.[0-9]\{2\}\.//g}' adat
